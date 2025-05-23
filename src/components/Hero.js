8125a127ad593ad3df4e0b617c5612dd41b16740
Miranda Morris <juniperistic@gmail.com>
2025-05-23 09:06:56 -0400

    Remove Small Text on Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -21,9 +21,10 @@ function Hero() {
       >
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-neutral-content text-center">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">CompSci Student</h1>
-            <p className="mb-5"> Hi, I'm Miranda</p>
+          <div className="max-w-lg">
+            <h1 className="mb-5 text-5xl font-bold">
+              Software Engineer & Tech Enthusiast
+            </h1>
             <nav>
               <div className="grid grid-flow-col gap-4 social-links">
                 <a
