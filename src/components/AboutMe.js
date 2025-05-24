6f0bce42886a114e02e5ce379b19c03eb53737c9
Miranda Morris <juniperistic@gmail.com>
2025-05-23 21:56:26 -0400

    Update AboutMe Text

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -24,7 +24,7 @@ function AboutMe() {
             name="my_tabs_1"
             role="tab"
             className="tab"
-            aria-label="IDES / Tools"
+            aria-label="IDEs / Tools"
             defaultChecked
           />
 
@@ -127,3 +127,110 @@ function AboutMe() {
   );
 }
 export default AboutMe;
+
+{
+  /* <div role="tablist" className="tabs tabs-bordered">
+          <input
+            type="radio"
+            name="my_tabs_1"
+            role="tab"
+            className="tab"
+            aria-label="IDES / Tools"
+            defaultChecked
+          />
+
+          <div role="tabpanel" className="tab-content">
+            <ul>
+              <li>
+                <span>Visual Studio / Visual Studio Code</span>
+              </li>
+              <li>
+                <span>Azure Services (Azure DevOps, Azure OpenAI, etc)</span>
+              </li>
+              <li>
+                <span>Jupyter Notebooks</span>
+              </li>
+              <li>
+                <span>Godot Engine</span>
+              </li>
+              <li>
+                <span>Unity Hub</span>
+              </li>
+              <li>
+                <span>Aseprite</span>
+              </li>
+              <li>
+                <span>PowerBI</span>
+              </li>
+              <li>
+                <span>GitHub</span>
+              </li>
+              <li>
+                <span>Matlab</span>
+              </li>
+            </ul>
+          </div>
+
+          <input
+            type="radio"
+            name="my_tabs_1"
+            role="tab"
+            className="tab"
+            aria-label="Languages"
+          />
+          <div role="tabpanel" className="tab-content">
+            <ul>
+              <li>
+                <span>C#</span>
+              </li>
+              <li>
+                <span>Python</span>
+              </li>
+              <li>
+                <span>Java</span>
+              </li>
+              <li>
+                <span>Assembly</span>
+              </li>
+              <li>
+                <span>HTML</span>
+              </li>
+              <li>
+                <span>CSS</span>
+              </li>
+              <li>
+                <span>SASS</span>
+              </li>
+              <li>
+                <span>JavaScript</span>
+              </li>
+              <li>
+                <span>React</span>
+              </li>
+            </ul>
+          </div>
+          <input
+            type="radio"
+            name="my_tabs_1"
+            role="tab"
+            className="tab"
+            aria-label="APIs"
+          />
+          <div role="tabpanel" className="tab-content">
+            <ul>
+              <li>
+                <span>Spotify API</span>
+              </li>
+              <li>
+                <span>YouTube API</span>
+              </li>
+              <li>
+                <span>Discord API</span>
+              </li>
+              <li>
+                <span>OpenAI API</span>
+              </li>
+            </ul>
+          </div>
+        */
+}
